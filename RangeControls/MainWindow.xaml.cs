7f6899c2d7921676c8966dc4c523d53724d13d5a
nbourre <nbourrereg@hotmail.com>
2020-01-29 16:48:50 -0500

    Premier commit

---
+++ RangeControls/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RangeControls
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            SliderValueA.Text = ((Slider)sender).Value.ToString();
+        }
+
+        private void Slider_ValueChanged_1(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            SliderValueB.Text = ((Slider)sender).Value.ToString();
+        }
+
+        private void StartLongRunningTask_Click(object sender, RoutedEventArgs e)
+        {
+            myProgressBar.Value = 0;
+            Task.Run(() =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Thread.Sleep(150);
+                    Application.Current.Dispatcher.Invoke(() => { myProgressBar.Value += 10; });
+                }
+            });
+        }
+    }
+}
---
+++ xaml_basics/Buttons.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace xaml_basics
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Buttons : Window
+    {
+        public Buttons()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void OrdinaryButton_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("Bouton cliqué");
+        }
+
+        private void RepeatButton_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("Bouton maintenu");
+        }
+
+        private void ToggleButton_Click(object sender, RoutedEventArgs e)
+        {
+            ToggleButton btn = (ToggleButton)sender;
+            
+            Debug.WriteLine($"Bouton état {btn.IsChecked}");
+        }
+
+        private void chkBox_Checked(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine($"Case cochée");
+        }
+
+        private void chkBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine($"Case décochée");
+        }
+
+        private void chkBox_Indeterminate(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine($"Case indéfinie");
+        }
+    }
+}
